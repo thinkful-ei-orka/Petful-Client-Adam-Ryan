1228181a3e35f21dc17ede62fab1f48daf264029
Adam Brown <adam.edward.brown@gmail.com>
2020-07-24 16:23:44 -0400

    Pet component completed

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,1 +1,29 @@
 import React from "react";
+import PetService from "../services/PetService";
+
+export default function Pet(props) {
+  return (
+    <section>
+      <h3>{props.pet.name}</h3>
+      <img src={props.pet.imageURL} alt={`${props.pet.description}`} />
+      <table>
+        <tr>
+          <th>Name</th>
+          <tr>{props.pet.age}</tr>
+        </tr>
+        <tr>
+          <th>Gender</th>
+          <tr>{props.pet.gender}</tr>
+        </tr>
+        <tr>
+          <th>Breed</th>
+          <tr>{props.pet.breed}</tr>
+        </tr>
+        <tr>
+          <th>Story</th>
+          <tr>{props.pet.story}</tr>
+        </tr>
+      </table>
+    </section>
+  );
+}
