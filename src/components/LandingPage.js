7f35036b442139a52f71a8660cfaa838fc1183ef
Ryan Vargas <i.am.ryan.v@gmail.com>
2020-07-24 09:08:24 -0700

    Initial client commit

---
+++ src/components/LandingPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+class LandingPage extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Welcome to Petful!</h1>
+        <p>Grab Animal</p>
+        <p>Click Button to Grab Animal</p>
+        <p>thanku</p>
+      </>
+    );
+  }
+}
